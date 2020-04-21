15af0bd93b3fb0badb4a6b8f8e232e30da8b445d
Ugurcan Kaya <ugurcankaa@icloud.com>
2020-04-21 17:12:47 +0300

    beta

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            searchTextField.placeholder = "Type something"
+            return false
+        }else {
+            return true
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //use searchTextField.text  to get the weather for that city
+        searchTextField.text = ""
 
+    }
+    
 }
 
